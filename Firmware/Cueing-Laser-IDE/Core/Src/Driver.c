3438b29c82d4215e2367c0d2918092264cca9a1f
Triet <76050658+trietmt9@users.noreply.github.com>
2024-05-16 17:56:14 +0800

    minor update for driver lib and servo lib

--- Firmware/Cueing-Laser-IDE/Core/Src/Driver.c
+++ Firmware/Cueing-Laser-IDE/Core/Src/Driver.c
@@ -1,25 +1,57 @@
-#include <Driver.h>
+/**
+ * @file
+ * @brief Driver file for controlling LEDs and Vibrator using PWM.
+ */
+
+/**
+ * @defgroup Driver_Module Driver Module
+ * @ingroup STM32F4xx_HAL_Driver
+ * @brief This file contains functions that allow you to control LEDs and Vibrator using PWM.
+*/
+#include "Driver.h"
 extern TIM_TypeDef htim3;
+
+/**
+ * @brief Controls the brightness of LED1.
+ *
+ * @param[in] brightness The brightness level of LED1.
+ */
 void LED1_Brightness(PWM_signal brightness)
 {
     TIM3->CCR1 = brightness;
     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
 }
 
+/**
+ * @brief Controls the brightness of LED2.
+ *
+ * @param[in] brightness The brightness level of LED2.
+ */
 void LED2_Brightness(PWM_signal brightness)
 {
     TIM3->CCR2 = brightness;
     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
 }
 
+/**
+ * @brief Controls the brightness of LED3.
+ *
+ * @param[in] brightness The brightness level of LED3.
+ */
 void LED3_Brightness(PWM_signal brightness)
 {
     TIM3->CCR3 = brightness;
     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
 }
 
+/**
+ * @brief Controls the vibration intensity of the vibrator.
+ *
+ * @param[in] pwm The vibration intensity level.
+ */
 void Vibrator_Control(PWM_signal pwm)
 {
     TIM3->CCR4 = pwm;
     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
 }
+
--- Firmware/Cueing-Laser-IDE/Core/Src/Servo.c
+++ Firmware/Cueing-Laser-IDE/Core/Src/Servo.c
@@ -1,32 +1,66 @@
+/**
+ * @file servo_control.c
+ * @brief Servo control functions
+ *
+ * This file contains functions to control the servos using PWM signals.
+ *
+ * @author Your Name
+ * @date 2022-01-01
+ */
+
 #include<Servo.h>
 extern TIM_TypeDef htim4;
 
-void Servo1_setAngle(angle_t angle)
-{
+/**
+ * @brief Sets the angle for Servo 1.
+ *
+ * This function sets the angle for Servo 1 by calculating the corresponding PWM value and writing it to the TIM4 CCR1 register.
+ *
+ * @param angle The desired angle for Servo 1 in degrees.
+ */
+void Servo1_setAngle(angle_t angle) {
     uint8_t pwm;
-    pwm = (angle*10)/180;
+    pwm = (angle * 10) / 180;
     TIM4->CCR1 = pwm;
-    HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_1);
+    HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
 }
 
-void Servo2_setAngle(angle_t angle)
-{
+/**
+ * @brief Sets the angle for Servo 2.
+ *
+ * This function sets the angle for Servo 2 by calculating the corresponding PWM value and writing it to the TIM4 CCR2 register.
+ *
+ * @param angle The desired angle for Servo 2 in degrees.
+ */
+void Servo2_setAngle(angle_t angle) {
     uint8_t pwm;
     pwm = (angle*10)/180;
     TIM4->CCR2 = pwm;
     HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_2);
 }
 
-void Servo3_setAngle(angle_t angle)
-{
+/**
+ * @brief Sets the angle for Servo 3.
+ *
+ * This function sets the angle for Servo 3 by calculating the corresponding PWM value and writing it to the TIM4 CCR3 register.
+ *
+ * @param angle The desired angle for Servo 3 in degrees.
+ */
+void Servo3_setAngle(angle_t angle) {
     uint8_t pwm;
     pwm = (angle*10)/180;
     TIM4->CCR3 = pwm;
     HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_3);
 }
 
-void Servo4_setAngle(angle_t angle)
-{
+/**
+ * @brief Sets the angle for Servo 4.
+ *
+ * This function sets the angle for Servo 4 by calculating the corresponding PWM value and writing it to the TIM4 CCR4 register.
+ *
+ * @param angle The desired angle for Servo 4 in degrees.
+ */
+void Servo4_setAngle(angle_t angle) {
     uint8_t pwm;
     pwm = (angle*10)/180;
     TIM4->CCR4 = pwm;
